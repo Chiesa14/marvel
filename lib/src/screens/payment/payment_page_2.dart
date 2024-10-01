d99bda1da3ccda9ed92c7e36edc25085e12b7033
Chiesa14 <remychiesa14@gmail.com>
2024-10-01 09:54:08 +0200

    Payment page fixes

--- lib/src/screens/payment/payment_page_2.dart
+++ lib/src/screens/payment/payment_page_2.dart
@@ -14,8 +14,10 @@ class PaymentPage2 extends StatefulWidget {
 
 class _PaymentPage2State extends State<PaymentPage2> {
   final TextEditingController _firtNameController = TextEditingController();
-
-  int selectedPlan = 0;
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _cardNumberController = TextEditingController();
+  final TextEditingController _expDateController = TextEditingController();
+  final TextEditingController _cvvController = TextEditingController();
 
   void handleContinue(BuildContext context) {
     Navigator.push(
@@ -30,115 +32,117 @@ class _PaymentPage2State extends State<PaymentPage2> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Container(
-        height: screenHeight,
-        width: screenWidth,
-        color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                const NumberLine(
-                  selectedIndex: 2,
-                ),
-                Container(
-                  height: 1,
-                  color: Colors.white24,
-                ),
-                const SizedBox(height: 50),
-                SvgPicture.asset(
-                  AppAssets.lgLogo,
-                ),
-                const SizedBox(height: 20),
-                const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Fill your Credit / Debit Card Details',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30),
-                      ),
-                    ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: screenHeight,
+          width: screenWidth,
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const NumberLine(
+                    selectedIndex: 2,
                   ),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    children: [
-                      Input(
-                        controller: _firtNameController,
-                        placeholder: 'First Name',
-                        keyboardType: TextInputType.text,
-                      ),
-                      const SizedBox(height: 10),
-                      Input(
-                        controller: _firtNameController,
-                        placeholder: 'Last Name',
-                        keyboardType: TextInputType.text,
-                      ),
-                      const SizedBox(height: 20),
-                      Input(
-                        controller: _firtNameController,
-                        placeholder: 'Card Number',
-                        keyboardType: TextInputType.number,
-                      ),
-                      const SizedBox(height: 10),
-                      Input(
-                        controller: _firtNameController,
-                        placeholder: 'Expiration Date (MM/YY)',
-                        keyboardType: TextInputType.datetime,
-                      ),
-                      const SizedBox(height: 10),
-                      Input(
-                        controller: _firtNameController,
-                        placeholder: 'Security Code (CVV)',
-                        keyboardType: TextInputType.datetime,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "Movies & Series \$20/month",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          const SizedBox(width: 40),
-                          GestureDetector(
-                            onTap: () {
-                              int count = 0;
-                              Navigator.popUntil(context, (route) {
-                                return count++ == 2;
-                              });
-                            },
-                            child: const Text(
-                              "Change",
-                              style: TextStyle(color: Color(0xFFED1B24)),
+                  Container(height: 1, color: Colors.white24),
+                  const SizedBox(height: 20),
+                  SvgPicture.asset(AppAssets.lgLogo),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Fill your Credit / Debit Card Details',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        Input(
+                          controller: _firtNameController,
+                          placeholder: 'First Name',
+                          keyboardType: TextInputType.text,
+                        ),
+                        const SizedBox(height: 10),
+                        Input(
+                          controller: _lastNameController,
+                          placeholder: 'Last Name',
+                          keyboardType: TextInputType.text,
+                        ),
+                        const SizedBox(height: 20),
+                        Input(
+                          controller: _cardNumberController,
+                          placeholder: 'Card Number',
+                          keyboardType: const TextInputType.numberWithOptions(
+                              decimal: false, signed: false),
+                        ),
+                        const SizedBox(height: 10),
+                        Input(
+                          controller: _expDateController,
+                          placeholder: 'Expiration Date (MM/YY)',
+                          keyboardType: TextInputType.datetime,
+                        ),
+                        const SizedBox(height: 10),
+                        Input(
+                          controller: _cvvController,
+                          placeholder: 'Security Code (CVV)',
+                          keyboardType: TextInputType.number,
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const SizedBox(width: 5),
+                            const Text(
+                              "Movies & Series \$20/month",
+                              style: TextStyle(color: Colors.white),
                             ),
-                          )
-                        ],
-                      )
-                    ],
+                            const SizedBox(width: 40),
+                            GestureDetector(
+                              onTap: () {
+                                int count = 0;
+                                Navigator.popUntil(context, (route) {
+                                  return count++ == 2;
+                                });
+                              },
+                              child: const Text(
+                                "Change",
+                                style: TextStyle(color: Color(0xFFED1B24)),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Button(
-                title: "Continue",
-                onPressed: selectedPlan == 0
-                    ? () => {}
-                    : () => handleContinue(context),
-                backgroundColor: selectedPlan != 0 ? true : false,
-                textStyle: const TextStyle(color: Colors.white, fontSize: 20),
-                height: 50,
+                ],
               ),
-            ),
-          ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Button(
+                  title: "Continue",
+                  onPressed: _cardNumberController.text.isNotEmpty &&
+                          _cvvController.text.isNotEmpty &&
+                          _expDateController.text.isNotEmpty
+                      ? () => {}
+                      : () => handleContinue(context),
+                  backgroundColor: _cardNumberController.text.isNotEmpty &&
+                          _cvvController.text.isNotEmpty &&
+                          _expDateController.text.isNotEmpty
+                      ? true
+                      : false,
+                  textStyle: const TextStyle(color: Colors.white, fontSize: 20),
+                  height: 50,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
